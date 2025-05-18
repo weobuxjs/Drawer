2fa5606606c7337ca29c998a67f9ae56704ac139
WeoBuXCS <dangquanghuy551@gmail.com>
2025-05-18 16:36:02 +0700

    Nothing

--- src/Core/GUI_Engine/GUI_Engine.h
+++ src/Core/GUI_Engine/GUI_Engine.h
@@ -4,9 +4,11 @@
 #include "../Entity.h"
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
-void Init(const char* title, int wndowWidth, int windowHeight, int windowPosX, int windowPosY);
+void init(const char* title, int wndowWidth, int windowHeight, int windowPosX, int windowPosY);
 void render(Entity* entity);
 void render(SDL_Texture *tex, SDL_Rect *src, SDL_Rect *dst, int rotation);
 void clearScreen(SDL_Color screenColor = {255, 255, 255, 255});
+void drawLine(int x1, int y1, int x2, int y2);
+int drawRect(int x, int y, int w, int h);
 
 #endif
