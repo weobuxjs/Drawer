bc02e48c02b8b690eb82d770b1f7dfee99d57142
WeoBuXCS <dangquanghuy551@gmail.com>
2025-05-25 15:49:43 +0700

    errands

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@ int main(int argc, char *argv[])
     Instance* mainInstance = new Instance();
     SDL_Init(SDL_INIT_EVERYTHING);
     mainInstance->run();
-    mainInstance->cleanup();
+    mainInstance->cleanUp();
     return 0;
 }
